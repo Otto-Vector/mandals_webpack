1e87a7ad18a7a20de450c6225b73afa879cc7ecf
otto-vector <otto-vector@yandex.ru>
2020-09-24 22:17:39 +0300

    maximum fix to_one_fibbonachi fn

--- src/js/modules/support.js
+++ src/js/modules/support.js
@@ -5,10 +5,7 @@
 
 //функция сложения чисел к одному числу фибоначчи
 function to_one_fibbonachi_digit(number_in_fn) {//передаётся числовое значение
-
-    number_in_fn = Math.abs(+number_in_fn%9) //на всякий случай перевод из отрицательного в абсолютное значение с нумеризацией
-
-    return number_in_fn == 0 ? 9 : number_in_fn
+    return number_in_fn%9 || 9
   }//возвращает одну цифру по фибоначчи
 
 //функция
@@ -21,18 +18,18 @@ function modification_to_normal(string_from_user_input, string_by_default) {
 //принимает две строки, string_from_user_input - на обработку
 //string_by_default - на замену, если string_from_user_input оказалась false
 
-  return  (
-            !string_from_user_input ||
-            !string_from_user_input.trim() ||
-            +string_from_user_input == 0
-          ) ? //проверка string_from_user_input на значения приводящие к false (в том числе пустая строка после сброса пробелов)
+          
+  return  (//проверка string_from_user_input 
+            !string_from_user_input || // на значения приводящие к false
+            !string_from_user_input.trim() || // (в том числе пустая строка после сброса пробелов)
+            +string_from_user_input == 0 //бессмысленные нули
+          ) ? 
               //присваивается значение по умолчанию //и (на всякий случай) обрабатывается и оно
               modification_to_normal( string_by_default, "0123456789" )
             : 
               string_from_user_input
                 .delete_all_spaces() //убираем все пробелы
-                // .slice( 0, max_input_length )        //обрезание более 30ти символов
                 .toLowerCase()     //убираем верхний регистр
-}//возвращает обработанную строку без пробелов меньше max_input_length символов в нижнем регистре либо обработанную тестовую строку
+}//возвращает обработанную строку без пробелов в нижнем регистре либо обработанную тестовую строку
 
 export {to_one_fibbonachi_digit, to_one_eleven_digit, modification_to_normal}
